43cca54e46f72b2ae8fb4c5246d8bdcb8b6ef66d
Loes Oldhoff <loesoldhoff@hotmail.com>
2024-09-17 12:47:10 +0200

    Strings naar hashtag comments voor duidelijke kleuren

--- Deel_1_comments_en_prints.py
+++ Deel_1_comments_en_prints.py
@@ -1,29 +1,28 @@
-"""
-Welkom bij Python!
-Python is een programmeertaal waardoor wij computers instructies kunnen
-geven om problemen voor ons op te lossen.
+# Welkom bij Python!
+# Python is een programmeertaal waardoor wij computers instructies kunnen
+# geven om problemen voor ons op te lossen
 
-In dit programma is bepaalde tekst gekleurd
-Groene tekst tussen driedubbele aanhalingstekens zijn comments.
-Dit is niet voor de computer, maar voor mensen die de code lezen.
+# In dit programma is bepaalde tekst gekleurd
+# Groene tekst tussen aanhalingstekens zijn 'strings' oftewel tekstdata
+# Zinnen achter hashtags zijn comments, die worden niet als code uitgevoerd
+# Dit is niet voor de computer, maar voor mensen die de code lezen
 
-Hieronder begint de code:
-"""
+# Hieronder begint de code:
+
 
 print("Hello world!")
 
-""" 
-De zin hierboven staat niet tussen hogekommas en zal dus 
-als enige uitgevoerd worden door Python
-'print' is hoe de computer terug naar ons kan praten
-Klik rechts op dit veld en selecteer 'run', kijk eens wat er gebeurt
-"""
 
-"""
-Kan je de computer zelf ook iets laten zeggen?
-Kopieer de code van hierboven en verander de groene tekst in iets anders
-(Zorg wel dat de aanhalingstekens blijven staan)
-"""
+# De zin hierboven staat niet tussen hogekommas en zal dus
+# als enige uitgevoerd worden door Python
+# 'print' is hoe de computer terug naar ons kan praten
+# Klik rechts op dit veld en selecteer 'run', kijk eens wat er gebeurt
+
+
+# Kan je de computer zelf ook iets laten zeggen?
+# Kopieer de code van hierboven en verander de groene tekst in iets anders
+# (Zorg wel dat de aanhalingstekens blijven staan)
+
 
 
 
